0ef3e6d8fb4660cc1c2c55becfe89fcbdc709729
Sarina Burch <sarinaburch52@gmail.com>
2020-08-25 15:59:45 -0400

    adding gif

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ ${answers.questons}
 
      
 
-    fs.writeFile("README.md", readMe, err => {
+    fs.writeFile(answers.projectName + "README.md", readMe, err => {
       if (err) {
           return console.log(err)
         }
